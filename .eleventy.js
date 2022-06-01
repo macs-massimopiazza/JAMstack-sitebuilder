694292316353d523d623f428ac87f7b4a1f05ceb
macs-massimopiazza <me@massimopiazza.com>
2022-06-01 11:01:30 +0200

    added homepage, jumbo component, featured post section, add dynamic featured post printing

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+    //Tell to 11ty what files to simply copy into public folder
+    eleventyConfig.addPassthroughCopy('./src/style.css')
+    eleventyConfig.addPassthroughCopy('./src/assets')
+
+    return {
+        dir: {
+            input: "src", //Folder with main site source files
+            output: "public" //Folder where the build will be created
+        }
+    }
+}
