8a066b2a60f2345d707676b7c55021c77f64eb70
macs-massimopiazza <me@massimopiazza.com>
2022-06-02 11:31:19 +0200

    netlify cms config

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     //Tell to 11ty what files to simply copy into public folder
     eleventyConfig.addPassthroughCopy('./src/style.css')
     eleventyConfig.addPassthroughCopy('./src/assets')
+    eleventyConfig.addPassthroughCopy('./src/admin')
 
     return {
         dir: {
