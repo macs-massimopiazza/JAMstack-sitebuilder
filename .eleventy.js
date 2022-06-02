754a5e3db0ace957f1262932d118be1dbc980e28
macs-massimopiazza <me@massimopiazza.com>
2022-06-02 12:14:33 +0200

    formatted date with filter & article snippet conditinal alternated background

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
     //Tell to 11ty what files to simply copy into public folder
     eleventyConfig.addPassthroughCopy('./src/style.css')
     eleventyConfig.addPassthroughCopy('./src/assets')
     eleventyConfig.addPassthroughCopy('./src/admin')
+
+    //formatting date
+    eleventyConfig.addFilter("postDate", (dateObj)=>{
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    })
 
     return {
         dir: {
